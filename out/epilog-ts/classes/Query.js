44a3c1a8bdf3f40f5709a216a648dbd9f851e3c5
pjames27 <44187608+pjames27@users.noreply.github.com>
2023-09-06 10:10:04 -0700

    Added ArbitraryQuery class and makeQuery function
    - Implemented the ArbitraryQuery class, which has no restrictions on the types of rules that can comprise it.
    - Implemented a makeQuery function which takes a query predicate and list of rules as input, and returns a Query object corresponding to the smallest class of the query that the input falls into. (e.g. a CQ will be a CQ object, even though it is also a UCQ)
        - If the query cannot be classified as a narrow implemented class, a corresponding ArbitraryQuery object will be returned by default

--- out/epilog-ts/classes/Query.js
+++ out/epilog-ts/classes/Query.js
@@ -1,5 +1,23 @@
 import { ERROR_RULE } from "./Rule.js";
-// Classification functions
+// ============================== General Query functions ==============================
+// Construct a Query object from the input
+// The type of the returned object will be the most specific class of query that the input falls into. (e.g. a CQ will be a CQ object, even though it is also a UCQ)
+// If the input does not fall into an implemented/distinguished class of query, returns an ArbitraryQuery object
+function makeQuery(queryPred, rules) {
+    // Temporary version of this, while CQ is the only non-Arbitrary implemented Query class
+    let defaultQuery = new ArbitraryQuery(queryPred, rules);
+    if (rules.length !== 1) {
+        return defaultQuery;
+    }
+    for (let subgoal of rules[0].body) {
+        if (subgoal.isNegated()) {
+            return defaultQuery;
+        }
+    }
+    // TODO: Check for interpreted predicates
+    return new ConjunctiveQuery(queryPred, rules[0]);
+}
+// =============== Classification functions ===============
 function isCQ(q) {
     // Note: temporary test, as we should also check the properties of the rules of the query, in case its class is too conservative
     return q instanceof ConjunctiveQuery;
@@ -19,6 +37,7 @@ class ConjunctiveQuery {
                 return;
             }
         }
+        // TODO: Check for interpreted predicates
         this.rule = rule;
         // Warn if the query predicate doesn't match the rule head
         if (this.queryPred.toString() !== this.rule.head.pred.toString()) {
@@ -29,5 +48,32 @@ class ConjunctiveQuery {
         return this.rule.toString();
     }
 }
-export { ConjunctiveQuery, isCQ, };
+// Every query can be an ArbitraryQuery - this is the most general class of query.
+class ArbitraryQuery {
+    constructor(queryPred, rules) {
+        this.queryPred = queryPred;
+        this.rules = rules;
+        // Check whether the query predicate doesn't match any rule head
+        let queryPredMatchesHead = false;
+        for (let rule of this.rules) {
+            if (this.queryPred.toString() === rule.head.pred.toString()) {
+                queryPredMatchesHead = true;
+                break;
+            }
+        }
+        // Warn if not
+        if (!queryPredMatchesHead) {
+            console.warn("Query predicate", this.queryPred.toString(), "doesn't appear in query:", this.toString());
+        }
+    }
+    toString() {
+        let str = "{\n";
+        for (let rule of this.rules) {
+            str += "\t" + rule.toString() + "\n";
+        }
+        str += "}";
+        return str;
+    }
+}
+export { ConjunctiveQuery, makeQuery, isCQ, };
 //# sourceMappingURL=Query.js.map
--- src/epilog-ts/classes/Query.ts
+++ src/epilog-ts/classes/Query.ts
@@ -7,7 +7,32 @@ interface Query {
     readonly rules: Rule[];
 }
 
-// Classification functions
+// ============================== General Query functions ==============================
+// Construct a Query object from the input
+// The type of the returned object will be the most specific class of query that the input falls into. (e.g. a CQ will be a CQ object, even though it is also a UCQ)
+// If the input does not fall into an implemented/distinguished class of query, returns an ArbitraryQuery object
+function makeQuery(queryPred: Predicate, rules: Rule[]) : Query {
+
+    // Temporary version of this, while CQ is the only non-Arbitrary implemented Query class
+    let defaultQuery: ArbitraryQuery = new ArbitraryQuery(queryPred, rules);
+
+
+    if (rules.length !== 1) {
+        return defaultQuery;
+    }
+
+    for (let subgoal of rules[0].body) {
+        if (subgoal.isNegated()) {
+            return defaultQuery
+        }
+    }
+
+    // TODO: Check for interpreted predicates
+
+    return new ConjunctiveQuery(queryPred, rules[0]);
+}
+
+// =============== Classification functions ===============
 
 function isCQ(q: Query) : q is ConjunctiveQuery {
     // Note: temporary test, as we should also check the properties of the rules of the query, in case its class is too conservative
@@ -15,7 +40,9 @@ function isCQ(q: Query) : q is ConjunctiveQuery {
 }
 
 
-// Classes
+// ============================== End General Query functions ==============================
+
+// ============================== Query Classes ==============================
 
 type CQ = ConjunctiveQuery;
 
@@ -40,6 +67,8 @@ class ConjunctiveQuery implements Query {
             }
         }
 
+        // TODO: Check for interpreted predicates
+
         this.rule = rule;
 
         // Warn if the query predicate doesn't match the rule head
@@ -53,11 +82,51 @@ class ConjunctiveQuery implements Query {
     }
 }
 
+// Every query can be an ArbitraryQuery - this is the most general class of query.
+class ArbitraryQuery implements Query {
+    readonly queryPred: Predicate;
+    readonly rules: Rule[];
+
+    constructor(queryPred: Predicate, rules: Rule[]) {
+        this.queryPred = queryPred;
+        this.rules = rules;
+
+        // Check whether the query predicate doesn't match any rule head
+        let queryPredMatchesHead = false;
+        for (let rule of this.rules) {
+            if (this.queryPred.toString() === rule.head.pred.toString()) {
+                queryPredMatchesHead = true;
+                break;
+            }
+        }
+
+        // Warn if not
+        if (!queryPredMatchesHead) {
+            console.warn("Query predicate", this.queryPred.toString(), "doesn't appear in query:",this.toString());
+        }
+    }
+
+    toString() : string {
+        let str = "{\n";
+
+        for (let rule of this.rules) {
+            str += "\t"+ rule.toString() +"\n"
+        }
+
+        str += "}"
+
+        return str;
+    }
+}
+
 export { 
     Query,
 
     
     CQ,
     ConjunctiveQuery,
+
+
+    makeQuery,
     isCQ,
 }
