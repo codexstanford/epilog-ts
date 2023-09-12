cf05c9bd37d39bee995162a28b6bd57ddc5880a9
pjames27 <44187608+pjames27@users.noreply.github.com>
2023-09-11 17:06:28 -0700

    Implemented quantifier stripping
    - Implemented in first-order-logic/transformations/general.ts the stripQuantifiers method which removes all quantifiers from the input formula. The Quantifiers and Variables of QuantifiedFormulae are removed, but their "formula" properties remain part of the formula.

--- out/first-order-logic/transformations/general.js
+++ out/first-order-logic/transformations/general.js
@@ -1,6 +1,48 @@
+import { Literal } from "../../epilog-ts/classes/Literal.js";
 import { Variable } from "../../epilog-ts/classes/Term.js";
+import { Biconditional } from "../classes/Biconditional.js";
+import { Conjunction } from "../classes/Conjunction.js";
+import { Disjunction } from "../classes/Disjunction.js";
+import { ERROR_FORMULA } from "../classes/Formula.js";
+import { Implication } from "../classes/Implication.js";
+import { Negation } from "../classes/Negation.js";
 import { QuantifiedFormula, Quantifier } from "../classes/QuantifiedFormula.js";
 import { getFreeVars } from "../utils/general.js";
+// Removes all quantifiers from the input formula.
+// The Quantifiers and Variables of QuantifiedFormulae are removed, but their "formula" properties remain part of the formula. 
+function stripQuantifiers(formula) {
+    if (formula instanceof Literal) {
+        return formula;
+    }
+    if (formula instanceof Negation) {
+        return new Negation(stripQuantifiers(formula.target));
+    }
+    if (formula instanceof Conjunction) {
+        let strippedConjuncts = [];
+        for (let conjunct of formula.conjuncts) {
+            strippedConjuncts.push(stripQuantifiers(conjunct));
+        }
+        return new Conjunction(strippedConjuncts);
+    }
+    if (formula instanceof Disjunction) {
+        let strippedDisjuncts = [];
+        for (let disjunct of formula.disjuncts) {
+            strippedDisjuncts.push(stripQuantifiers(disjunct));
+        }
+        return new Disjunction(strippedDisjuncts);
+    }
+    if (formula instanceof Implication) {
+        return new Implication(stripQuantifiers(formula.antecedent), stripQuantifiers(formula.consequent));
+    }
+    if (formula instanceof Biconditional) {
+        return new Biconditional(stripQuantifiers(formula.antecedent), stripQuantifiers(formula.consequent));
+    }
+    if (formula instanceof QuantifiedFormula) {
+        return stripQuantifiers(formula.formula);
+    }
+    console.error("Tried to strip quantifiers from a Formula without a valid type:", formula);
+    return ERROR_FORMULA;
+}
 // Returns the original formula prefixed with universal quantifiers for each of its free variables 
 function bindFreeVars(formula) {
     let freeVarSet = getFreeVars(formula);
@@ -14,5 +56,5 @@ function bindFreeVars(formula) {
     }
     return resultFormula;
 }
-export { bindFreeVars };
+export { stripQuantifiers, bindFreeVars };
 //# sourceMappingURL=general.js.map
