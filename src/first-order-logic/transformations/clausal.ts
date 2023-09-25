b2d516164f113a6a516a25c5ee62651db913764b
pjames27 <44187608+pjames27@users.noreply.github.com>
2023-09-25 15:36:53 -0700

    Implemented toClausal
    - Implemented the toClausal transformation for Formulae, which uses toPCNF to convert a Formula into a list of Clauses
        - Normalizes the Clauses, such that Negations become negated Literals, and all negative (positive) FALSE_LITERALS become positive (negative) TRUE_LITERALS
            - Normalizes to simplify the resolution procedure, as only Literals will occur in Clauses, and the FALSE_LITERAL will never occur, ensuring checking for refutation is as simple as looking for the empty clause.

---
+++ src/first-order-logic/transformations/clausal.ts
@@ -0,0 +1,94 @@
+import { ERROR_LITERAL, FALSE_LITERAL, Literal, TRUE_LITERAL } from "../../epilog-ts/classes/Literal.js";
+import { Clause } from "../classes/Clause.js";
+import { Conjunction } from "../classes/Conjunction.js";
+import { Disjunction } from "../classes/Disjunction.js";
+import { Formula } from "../classes/Formula.js";
+import { Negation } from "../classes/Negation.js";
+import { toPCNF } from "./pnf.js";
+
+// Normalize input via the following:
+    // A FALSE_LITERAL becomes a negated TRUE_LITERAL, and a negated FALSE_LITERAL becomes a positive TRUE_LITERAL
+function normalizeLiteralForClause(literal : Literal) : Literal {
+
+    if (literal.atom.toString() === FALSE_LITERAL.toString()) {
+        if (literal.isNegated()) {
+            return TRUE_LITERAL;
+        }
+
+        return new Literal(TRUE_LITERAL.atom, true);
+    }
+
+    return literal;
+}
+
+
+// Normalize input via the following:
+    // The negation of a Literal is converted into a negated Literal 
+function normalizeNegationForClause(negation : Negation) : Literal {
+
+    if (!(negation.target instanceof Literal)) {
+        console.error("Could not normalize negation for clause - its target was not a Literal:",negation.toString());
+        return ERROR_LITERAL;
+    }
+
+    return normalizeLiteralForClause(new Literal(negation.target.atom, true)); 
+}
+
+// Converts a Formula into a list of normalized clauses.
+    // Clauses are normalized via the helper functions normalizeLiteralForClause and normalizeNegationForClause
+    // Also converts negations of literals into literals with negative polarity.
+        // Normalization simplifies resolution by ensuring that only Literals occur in Clauses, and that only TRUE_LITERAL and its negated form will occur, never FALSE_LITERAL.
+        // Because of this, the only derived clause that indicates refutation is the empty clause.
+function toClausal(initialFormula: Formula) : Clause[] {
+    let cnfFormula = toPCNF(initialFormula, false);
+
+    if (!(cnfFormula instanceof Conjunction)) {
+        console.error("Could not convert formula to clausal form. toPCNF did not return a prefix-free cnf formula:",cnfFormula.toString());
+        return [];
+    }
+
+    let clauseList : Clause[] = [];
+
+    // Normalize the clauses
+    for (let conjunct of cnfFormula.conjuncts) {
+        if (conjunct instanceof Literal) {
+            clauseList.push(new Clause([normalizeLiteralForClause(conjunct)]));
+            continue;
+        }
+
+        if (conjunct instanceof Negation) {
+            clauseList.push(new Clause([normalizeNegationForClause(conjunct)]));
+            continue;
+        }
+
+        if (conjunct instanceof Disjunction) {
+            let literalList : Literal[] = [];
+
+            for (let disjunct of conjunct.disjuncts) {
+                if (disjunct instanceof Literal) {
+                    literalList.push(normalizeLiteralForClause(disjunct));
+                    continue;
+                }
+                if (disjunct instanceof Negation) {
+                    literalList.push(normalizeNegationForClause(disjunct));
+                    continue;
+                }
+
+                console.error("Could not convert formula to clausal form. toPCNF did not return a cnf formula - contained the following Disjunction with a non-Literal, non-Negation disjunct:",conjunct.toString());
+                return [];
+            }
+            clauseList.push(new Clause(literalList));
+            continue;
+        }
+
+        console.error("Could not convert formula to clausal form. toPCNF did not return a cnf formula - contained the following conjunct that was not a Literal, Negation, or Disjunction:",conjunct.toString());
+        return [];
+    }
+
+    return clauseList;
+}
+
+
+export {
+    toClausal
+}
